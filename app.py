2aaec578cfb7562bc00cf048b4de8c83971ac03d
luojin666 <huyaochengluojin@outlook.com>
2023-06-06 15:46:26 +0800

    更新

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', model_url='https://raw.githubusercontent.com/luojin666/Model-preview-and-quotation-system/master/models/xx.obj')
 
 if __name__ == '__main__':
     app.run()
-
